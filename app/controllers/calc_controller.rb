e7355e619630224844484b0214dac64b50ef58b4
Unoutsider <sashabukhov2@yandex.ru>
2022-11-15 17:18:08 +0300

    New version

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,9 +1,25 @@
+# frozen_string_literal: true
+
 class CalcController < ApplicationController
-  def input
-  end
+  def input; end
 
   def output
     v = params[:v].scan(/\d+/).map(&:to_i)
-    @result = v
+    @start = params[:v]
+    if v.length < 7
+      @result = 'Количество целых чисел в последовательности должно быть как минимум 7!'
+      [@result, @start]
+    else
+      @result1 = processor(v)
+      [@result1, @start]
+    end
+  end
+
+  def processor(arr_1)
+    edge = arr_1.minmax
+    relation = (Float(edge[0]) / Float(edge[1])).round(5)
+    place = arr_1.length % 7
+    arr_1[-1 - place] = relation
+    arr_1.join(' ')
   end
 end
